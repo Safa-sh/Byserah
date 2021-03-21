28a77ca6736ac6d47dab0ceb89b6e51440b263db
Meshael1998 <mes7ooo1918@gmail.com>
2021-03-21 20:02:55 +0300

    push git

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from PIL import ImageTk, Image
 from tkinter import filedialog
 import tkinter.messagebox as tmsg
 import mysql.connector as mysql
-from collections import OrderedDict
 import webbrowser as wb
 root = Tk()
 def designTem(Tk,app_width,app_height):
@@ -19,29 +18,6 @@ def designTem(Tk,app_width,app_height):
     my_canvas = Canvas(Tk, width=300, height=10, bg="white")
     my_canvas.create_line(0, 10, 300, 10, fill="purple")
     my_canvas.pack(pady=50)
-
-
-# def patientID_wind(titl):
-#     def checkSubmit():
-#      Id_p=patientID.get()
-#
-#     def on_click(event):
-#      patientID.configure(state=NORMAL)
-#      patientID.delete(0, END)
-#      patientID.unbind('<Button-1>', on_click_id)
-#
-#     patientID_window = Toplevel(root, bg="white")
-#     patientID_window.title(titl)
-#     labal1 = Label(patientID_window, text=titl, font=('Calibri', 14), bg="white")
-#     labal1.place(x=125, y=10)
-#     designTem(patientID_window, 350, 300)
-#     patientID=Entry(patientID_window,width=30,font=('Calibri', 12))
-#     patientID.insert(0, "Ente PatientID. ")
-#     patientID.configure(state=DISABLED)
-#     on_click_id = patientID.bind('<Button-1>', on_click)
-#     patientID.pack()
-#     submtbtn = Button(patientID_window, text="Submit", width=15, height=1, bg="white",command=checkSubmit())
-#     submtbtn.place(x=110, y=230)
 class testframe:
     def startCon(self):
          con = mysql.connect(
@@ -49,64 +25,44 @@ class testframe:
             port="3308",
             user="root",
             passwd="8977749",
-            database="basyrah")
+            database="basyrahpg")
 
          return con
-    def __init__(self,root,type_p):
+    def __init__(self,root,type_p,P_ID):
         self.patientTest_window= Toplevel(root, bg="white")
         self.patientTest_window .geometry('500x500')
         self.patientTest_window .title("patientTest_window")
-        # labal1 = Label(self.patientID_window, text="patientTest_window", font=('Calibri', 14), bg="white")
-        # labal1.place(x=90, y=5)
-        self.con = mysql.connect(
-            host="localhost",
-            port="3308",
-            user="root",
-            passwd="8977749",
-            database="basyrah")
         self.P_ID= StringVar()
-        self.type_p=int()
+        self.type_p=type_p
         self.View()
 
     def View(self):
-
         designTem(self.patientTest_window, 300, 300)
-        NewTestbtn = Button(self.patientTest_window, text="new test ", width=15, height=1, bg="white")
-        NewTestbtn.place(x=110, y=130)
-        showPreResbtn = Button(self.patientTest_window, text="show previous result", width=15, height=1, bg="white")
-        showPreResbtn.place(x=110, y=260)
+        NewTestbtn = Button(self.patientTest_window, text="new test ", width=17,height=2, bg="white",fg="purple")
+        NewTestbtn.place(x=90, y=90)
+        showPreResbtn = Button(self.patientTest_window, text="show previous result", width=17,height=2, bg="white",fg="purple")
+        showPreResbtn.place(x=90, y=160)
         if (self.type_p == 1):
             showPreResbtn.configure(state=DISABLED)
         else:
              showPreResbtn.configure(state=NORMAL)
 
-
-
-
-
-
-
-
 class  patientDB:
-
     def startCon(self):
-         con = mysql.connect(
-            host="localhost",
-            port="3308",
-            user="root",
-            passwd="8977749",
-            database="basyrah")
-
-         return con
-
+        con = mysql.connect(
+              host="localhost",
+              port="3308",
+              user="root",
+              passwd="8977749",
+              database="basyrahpg")
+        return con
     def __init__(self,root,title,chooseNu):
         self.patientID_window= Toplevel(root, bg="white")
-        self.patientID_window .geometry('500x500')
         self.patientID_window .title(title)
         labal1 = Label(self.patientID_window, text=title, font=('Calibri', 14), bg="white")
         labal1.place(x=90, y=5)
         self.P_ID= StringVar()
-        self.chooseNu=int()
+        self.chooseNu=chooseNu
         self.View()
     def View(self):
       designTem(self.patientID_window, 300, 300)
@@ -122,7 +78,7 @@ class  patientDB:
       patientIDEntry.pack()
       submtbtn = Button(self.patientID_window, text="Submit", width=15, height=1, bg="white",command=self.find_patientID)
       submtbtn.place(x=110, y=230)
-    # =============================find_patientID
+    #=============================find_patientID
     def find_patientID(self):
         con=self.startCon()
         Id_p=self.P_ID.get()
@@ -130,25 +86,35 @@ class  patientDB:
             tmsg.showinfo('insert', "Please Enter patient_id")
         else:
                cursor = con.cursor()
-               cursor.execute("SELECT * FROM patient WHERE patient_id='"+Id_p+"'")
+               cursor.execute("SELECT patient_id FROM patient WHERE patient_id='"+Id_p+"'")
                result = cursor.fetchall()
-               if result:
-                   if (self.chooseNu == 1):#new patient
+               print(self.P_ID.get())
+               if result and (self.chooseNu == 1):#new patient
                        tmsg.showerror("Error","Data Already Found")
-                   else:
-                    testframe(root,2)
-               else:
-                   if (self.chooseNu == 1):#new patient
-                    self.add_New_patient(Id_p)
-                    testframe(root, 1)
-                   else:
-                       tmsg.showerror("Error", "Data not Found")
-
-
+               elif not result and(self.chooseNu == 1):
+                   self.add_New_patient(Id_p)
+                   testframe(root, 1,Id_p)
+               elif result and (self.chooseNu == 2):#previous patient
+                      testframe(root,2,Id_p)
+               elif not result and(self.chooseNu == 2):#previous patient
+                  a=tmsg.askquestion("","do you add it as new patiant ?")
+                  if a=='yes':
+                      self.add_New_patient()
         con.close()
 
-    #def add_New_patient(self):
-
+    def add_New_patient(self,Id_p):
+       #try:
+        con = self.startCon()
+        cursor = con.cursor()
+        inserSQL="""INSERT INTO patient(patient_id,total_checkup) VALUES (%s,%s)"""
+        initlv=0
+        data=(Id_p,str(initlv))
+        res=cursor.execute(inserSQL,data)
+        if res:
+            print("donnne ")
+        #except EXCEPTION as err:
+        con.commit()
+        con.close()
 
 def  uploadImagebtnFunction():
     choose_window = Toplevel(root, bg='white')
@@ -157,9 +123,13 @@ def  uploadImagebtnFunction():
     welcomLebal.place(x=80, y=10)
     def click():
         if(option.get()==1):
+            choose_window.destroy()
             patientDB(root,"New patient",1)
+
         else:
+          choose_window.destroy()
           patientDB(root,"Previous patient",2)
+
       # checkID(ID)
     Label(choose_window,text="Are you :", font=('Calibri', 12),bg="white",justify="right").pack()
     option = IntVar()
@@ -167,9 +137,6 @@ def  uploadImagebtnFunction():
     Radiobutton(choose_window, text="Previous patient.",fg="purple" ,bg="white",font=('Calibri', 14), variable=option, value=2).pack()
     submtbtn = Button(choose_window,text="Submit",width=15,height=1,bg="white",command=click)
     submtbtn.place(x=110, y=230)
-
-
-
 def main():
     # Main window
     app_width = 800
@@ -203,8 +170,8 @@ def main():
         root.filename = filedialog.askopenfilename(initialdir="C: \Interfaces\images",
                                                    title="Select a result",
                                                    filetype=(("pdf files", "*.pdf"), ("all files", "*.*")))
-    Button(root, text="Upload Image",width=20,height=2,command=uploadImagebtnFunction,bg="white").place(x=90, y=450)
-    Button(root, text="Download result",width=20,height=2,command=donResbtnFunction,bg="white").place(x=550, y=450)
+    Button(root, text="Upload Image",font=('Calibri', 12),width=20,height=2,command=uploadImagebtnFunction,bg="white").place(x=70, y=450)
+    Button(root, text="Download result",font=('Calibri', 12),width=20,height=2,command=donResbtnFunction,bg="white").place(x=550, y=450)
     root.mainloop()
 
 if __name__ == '__main__':
